3e6b30f60abb2c9f9d090b24438bc40e60780808
hamsoter <omilzmil0w0@gmail.com>
2022-03-20 19:07:11 +0900

    Design: 새 일기장 추가시 scale 애니메이션 효과 적용

--- src/components/Diary/Diary.js
+++ src/components/Diary/Diary.js
@@ -1,6 +1,16 @@
-import { Box, Button, Flex, ListItem, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  isOpen,
+  useDisclosure,
+  onToggle,
+  ListItem,
+  Text,
+} from '@chakra-ui/react';
 import React from 'react';
 import { useState } from 'react/cjs/react.production.min';
+import { Fade, ScaleFade, Slide, SlideFade } from '@chakra-ui/react';
 
 const Diary = props => {
   const { name, id, lastRecord, color, title } = props;
@@ -16,24 +26,27 @@ const Diary = props => {
     h: ['400px', '230px', '250px'],
   };
 
+  const { isOpen, onToggle } = useDisclosure();
   // console.log
   return (
-    <ListItem {...liStyles}>
-      <Box bg={color.subColor} color={color.trextColor} p={2}>
-        <Text fontSize={['3xl', 'xl', 'lg']}>{title}</Text>
-        <Text fontSize={'xs'}>
-          마지막 업데이트 {lastRecord.toLocaleDateString()}
+    <ScaleFade in={true} initialScale={0.4} transition={0.1}>
+      <ListItem {...liStyles}>
+        <Box bg={color.subColor} color={color.trextColor} p={2}>
+          <Text fontSize={['3xl', 'xl', 'lg']}>{title}</Text>
+          <Text fontSize={'xs'}>
+            마지막 업데이트 {lastRecord.toLocaleDateString()}
+          </Text>
+        </Box>
+        <Text
+          fontWeight={'bold'}
+          fontSize={['md', 'sm', 'xs']}
+          color={color.subColor}
+          ml={'2'}
+        >
+          '{name}'의 일기
         </Text>
-      </Box>
-      <Text
-        fontWeight={'bold'}
-        fontSize={['md', 'sm', 'xs']}
-        color={color.subColor}
-        ml={'2'}
-      >
-        '{name}'의 일기
-      </Text>
-    </ListItem>
+      </ListItem>
+    </ScaleFade>
   );
 };
 
