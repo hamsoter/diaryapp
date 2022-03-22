47246fa59f8abd3d45c167f7d97dad5fea412805
hamsoter <omilzmil0w0@gmail.com>
2022-03-22 18:05:29 +0900

    Feat: 렌더 무한루프 에러 수정
    useEffect 메서드를 사용하여 렌더 순서를 지정

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,5 +1,5 @@
-import { Box, ChakraProvider, Container, Text, theme } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { Box, ChakraProvider, theme } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/DiaryLists/Header';
 import MainContent from '../components/UI/MainContent';
 
@@ -9,6 +9,8 @@ const Diary = props => {
 
   // 찾아낸 다이어리를 저장할 공간
   const [thisDiary, setThisDiary] = useState('');
+
+  const [notFoundFlag, setNotFoundFlag] = useState('false');
 
   // 현재 url의 uuid 잘라내기
   const thisParamId = window.location.pathname.split('/')[2];
@@ -22,26 +24,42 @@ const Diary = props => {
     return result;
   };
 
-  const init = () => {
+  // 렌더 무한루프 방지 순서 처리
+  useEffect(() => {
     if (findById()) {
       setThisDiary(findById());
+
+      console.log(thisDiary);
     } else {
       // url의 uuid 값을 찾지 못할 경우
-      // 아래 메서드가 먼저 실행되어 무한루프 에러 생김
       console.log('error!');
+      setNotFoundFlag(false);
     }
-  };
+  });
 
-  init();
+  let content = (
+    <>
+      <Header title={thisDiary.title} rightContent={'버튼들어갈자리'} />
+      <MainContent />
+    </>
+  );
+
+  // 존재하지 않는 주소로 들어갔을 때 컨텐츠 내용 변경
+  if (!notFoundFlag) {
+    content = (
+      <>
+        <Header title={'（￣□￣；） ... 페이지를 찾을 수 없어요! '} />
+        <MainContent>
+          잘못된 주소로 들어오셨네요! 도움을 드리지 못해 미안해요
+        </MainContent>
+      </>
+    );
+  }
 
   return (
     <ChakraProvider h={'100%'} theme={theme}>
       <Box h={'100%'} w={['100%', '500px', '750px']} m="auto">
-        <Header title={'책이름'} rightContent={'버튼들어갈자리'} />
-        <MainContent>
-          <Box>dd</Box>
-        </MainContent>
-        {/* <DiaryLists diaries={diaries}></DiaryLists> */}
+        {content}
       </Box>
     </ChakraProvider>
   );
